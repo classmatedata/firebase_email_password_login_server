3ee30a9a3c8658aaf8194f3c815199b10a11b71d
Irina <7764871+ilavva@users.noreply.github.com>
2024-06-27 23:28:22 +0300

    firebase login

---
+++ public/index.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+    document.querySelector('#signUpButton').addEventListener('click', registerNewUser);
+    document.querySelector('#logInButton').addEventListener('click', logInUser);
+    document.querySelector('#logOutButton').addEventListener('click', logOutUser);
+});
+
